699882e7eb17176eaa698bc0bfd1c323f5534fc8
ernesto-official02 <ernestanmol@gmail.com>
2025-07-12 19:23:15 +0530

    cv

--- client/src/Hero.jsx
+++ client/src/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           </p>
 
           <a
-            href="/assests/Ernest_Anmol_resume.pdf" // Change this to your actual CV file path
+            href="/Ernest_Anmol_resume.pdf" // Change this to your actual CV file path
             download
             className="inline-block mt-6 px-6 py-3 bg-red-500 text-white font-medium rounded-full hover:bg-red-600 transition duration-300"
           >

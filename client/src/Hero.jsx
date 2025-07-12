3fe43f474c89b1ba517b55c291edfc6a7f328e18
ernesto-official02 <ernestanmol@gmail.com>
2025-07-12 19:33:38 +0530

    cv

--- client/src/Hero.jsx
+++ client/src/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
           <a
             href="https://drive.google.com/file/d/14cTPwkfitWyaUmxkbsB8uvv-s2j_IrUP/view?usp=sharing" // Change this to your actual CV file path
-            download
+            download target="_blank"
             className="inline-block mt-6 px-6 py-3 bg-red-500 text-white font-medium rounded-full hover:bg-red-600 transition duration-300"
           >
             Download CV
